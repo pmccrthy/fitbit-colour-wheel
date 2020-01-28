a619b3197fcc4f2f1051527eb8f7fc8127441148
Paul McCarthy <pmccrthy@protonmail.com>
2020-01-28 10:57:16 +0000

    Update to v2. Increased the size of the colours to workaround the 31 pixel limit for the height of an arc.

--- app/index.js
+++ app/index.js
@@ -1,216 +1,157 @@
-/*
- * Entry point for the watch app
- */
 import document from "document";
 
 var blnBasicMode = true;
-var intCurrentMode = 0;
-var strBaseColour = "";
-const intComplimentary = 6;
-const intSplitComplimentary1 = 5;
-const intSplitComplimentary2 = 7;
-const intBasicModeMax = 4;
-const intAdvModeMax = 5;
+var intCurMode = 0;
+var strBaseCol = "";
+const COMP = 6;
+const SPLITCOMP1 = 5;
+const SPLITCOMP2 = 7;
+const BASICMODEMAX = 4;
+const ADVMODEMAX = 5;
+const MODETEXT = document.getElementById("txtMode");
 
-var arrAllColours = ["y", "yg", "g", "bg", "b", "bv", "v", "rv", "r", "ro", "o", "yo"];
-var arrPriColours = ["y", "r", "b"];
-var arrSecColours = ["g", "v", "o"];
-var arrTerColours = ["yg", "bg", "bv", "rv", "ro", "yo"]
+var arrCols = ["y", "yg", "g", "bg", "b", "bv", "v", "rv", "r", "ro", "o", "yo"];
 
-function hideAllColours() {
-  arrAllColours.forEach((col, index) => { document.getElementById(col).style.display="none"; });                      
-  document.getElementById("txtMode").text="";
+const arrColsLen = arrCols.length
+
+function hideAll() {                     
+  document.getElementsByClassName("segment").forEach(function(e) {e.style.display="none";} );
+  MODETEXT.text="";
 }
 
-function showAllColours() {
-  arrAllColours.forEach((col, index) => { document.getElementById(col).style.display="inline"; }); 
-  document.getElementById("txtMode").text="All Colours";
+function showBase(strGroup, strTitle) {
+  hideAll();
+  document.getElementsByClassName(strGroup).forEach(function(e) {e.style.display="inline";} );
+  MODETEXT.text=strTitle; 
 }
 
-function showPrimaryColours() {
-  hideAllColours();
-  arrPriColours.forEach((col, index) => { document.getElementById(col).style.display="inline"; }); 
-  document.getElementById("txtMode").text="Primary Colours";
+function showComp(strBaseCol) {
+  hideAll();
+  document.getElementsByClassName(strBaseCol).forEach(function(e) {e.style.display="inline";} );
+  document.getElementsByClassName(arrCols[(arrCols.indexOf(strBaseCol) + COMP) % arrColsLen]).forEach(function(e) {e.style.display="inline";} );
+  MODETEXT.text="Complimentary";
 }
 
-function showSecondaryColours() {
-  hideAllColours();
-  arrSecColours.forEach((col, index) => { document.getElementById(col).style.display="inline"; }); 
-  document.getElementById("txtMode").text="Secondary Colours";
+function showSplitComp(strBaseCol) {
+  hideAll();
+  document.getElementsByClassName(strBaseCol).forEach(function(e) {e.style.display="inline";} );
+  document.getElementsByClassName(arrCols[(arrCols.indexOf(strBaseCol) + SPLITCOMP1) % arrColsLen]).forEach(function(e) {e.style.display="inline";} );
+  document.getElementsByClassName(arrCols[(arrCols.indexOf(strBaseCol) + SPLITCOMP2) % arrColsLen]).forEach(function(e) {e.style.display="inline";} ); 
+  MODETEXT.text="Split Complimentary";
 }
 
-function showTertiaryColours() {
-  hideAllColours();
-  arrTerColours.forEach((col, index) => { document.getElementById(col).style.display="inline"; }); 
-  document.getElementById("txtMode").text="Tertiary Colours";
-}
-
-function showSingleColour(strBaseColour) {
-  hideAllColours();
-  document.getElementById(strBaseColour).style.display="inline";
-  document.getElementById("txtMode").text="Single Colour";
-}
-
-function showComplimentaryColours(strBaseColour) {
-  hideAllColours();
-  document.getElementById(strBaseColour).style.display="inline";
-  document.getElementById(arrAllColours[(arrAllColours.indexOf(strBaseColour) + intComplimentary) % arrAllColours.length]).style.display="inline";
-  document.getElementById("txtMode").text="Complimentary";
-}
-
-function showSplitComplimentaryColours(strBaseColour) {
-  hideAllColours();
-  document.getElementById(strBaseColour).style.display="inline";
-  document.getElementById(arrAllColours[(arrAllColours.indexOf(strBaseColour) + intSplitComplimentary1) % arrAllColours.length]).style.display="inline";
-  document.getElementById(arrAllColours[(arrAllColours.indexOf(strBaseColour) + intSplitComplimentary2) % arrAllColours.length]).style.display="inline";
-  document.getElementById("txtMode").text="Split Complimentary";
-}
-
-function showAnalgousColoursRight(strBaseColour) {
-  hideAllColours();
+function showAnalogousRight(strBaseCol) {
+  hideAll();
   for(var i=0; i<4; i++) {
-    document.getElementById(arrAllColours[(arrAllColours.indexOf(strBaseColour) + i) % arrAllColours.length]).style.display="inline";
+    document.getElementsByClassName(arrCols[(arrCols.indexOf(strBaseCol) + i) % arrColsLen]).forEach(function(e) {e.style.display="inline";} );
   }
-  document.getElementById("txtMode").text="Analgous R";
+  MODETEXT.text="Analogous R";
 }
 
-function showAnalgousColoursLeft(strBaseColour) {
-  hideAllColours();
+function showAnalogousLeft(strBaseCol) {
+  hideAll();
   for(var i=0; i<4; i++) {
-    document.getElementById(arrAllColours[(arrAllColours.indexOf(strBaseColour) - i + arrAllColours.length) % arrAllColours.length]).style.display="inline";
+    document.getElementsByClassName(arrCols[(arrCols.indexOf(strBaseCol) - i + arrColsLen) % arrColsLen]).forEach(function(e) {e.style.display="inline";} );
   }
-  document.getElementById("txtMode").text="Analgous L";
+  MODETEXT.text="Analogous L";
 }
 
-function toggleMode(strBaseColour) {
+function toggleMode(strBaseCol) {
   blnBasicMode = !blnBasicMode;
-  intCurrentMode = 0;
+  intCurMode = 0;
   
   if (blnBasicMode) {
-    showAllColours();
+    showBase("segment", "All Colours");
   } else {
-    showSingleColour(strBaseColour);
+    showBase(strBaseCol, "Single");
   }
 }
 
-// Physical buttons event handler
 document.onkeypress = function(e) {
   if (blnBasicMode) {
     switch (e.key) {
       case "up":
-        intCurrentMode = (++intCurrentMode) % intBasicModeMax;
+        intCurMode = ++intCurMode % BASICMODEMAX;
         break;
       case "down":
-        intCurrentMode = ((--intCurrentMode) + intBasicModeMax) % intBasicModeMax;
+        intCurMode = (--intCurMode + BASICMODEMAX) % BASICMODEMAX;
         break;
       case "back":
         break;
     }
        
-    switch (intCurrentMode) {
+    switch (intCurMode) {
       case 0:
-        showAllColours();
+        showBase("segment", "All Colours");
         break;
       case 1:
-        showPrimaryColours();
+        showBase("pri", "Primary");
         break;
       case 2:
-        showSecondaryColours();
+        showBase("sec", "Secondary");
         break;
       case 3:
-        showTertiaryColours();
+        showBase("ter", "Tertiary");
         break;
     }
   } else {
     switch (e.key) {
       case "up":
-        intCurrentMode = (++intCurrentMode) % intAdvModeMax;
+        intCurMode = ++intCurMode % ADVMODEMAX;
         break;
       case "down":
-        intCurrentMode = ((--intCurrentMode) + intAdvModeMax) % intAdvModeMax;
+        intCurMode = (--intCurMode + ADVMODEMAX) % ADVMODEMAX;
         break;
       case "back":
         break;
     }
         
-    switch (intCurrentMode) {
+    switch (intCurMode) {
       case 0:
-        showSingleColour(strBaseColour);
+        showBase(strBaseCol, "Single");
         break;
       case 1:
-        showComplimentaryColours(strBaseColour);
+        showComp(strBaseCol);
         break;
       case 2:
-        showSplitComplimentaryColours(strBaseColour);
+        showSplitComp(strBaseCol);
         break;
       case 3:
-        showAnalgousColoursRight(strBaseColour);
+        showAnalogousRight(strBaseCol);
         break;
       case 4:
-        showAnalgousColoursLeft(strBaseColour);
+        showAnalogousLeft(strBaseCol);
         break;
     }   
   }
 }
 
-document.getElementById("y").onclick = function(e) {
-  strBaseColour="y";
-  toggleMode(strBaseColour);
+function segment_click(strCol) {
+  strBaseCol=strCol;
+  toggleMode(strBaseCol);
 }
 
-document.getElementById("yg").onclick = function(e) {
-  strBaseColour="yg";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("g").onclick = function(e) {
-  strBaseColour="g";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("bg").onclick = function(e) {
-  strBaseColour="bg";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("b").onclick = function(e) {
-  strBaseColour="b";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("bv").onclick = function(e) {
-  strBaseColour="bv";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("v").onclick = function(e) {
-  strBaseColour="v";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("rv").onclick = function(e) {
-  strBaseColour="rv";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("r").onclick = function(e) {
-  strBaseColour="r";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("ro").onclick = function(e) {
-  strBaseColour="ro";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("o").onclick = function(e) {
-  strBaseColour="o";
-  toggleMode(strBaseColour);
-}
-
-document.getElementById("yo").onclick = function(e) {
-  strBaseColour="yo";
-  toggleMode(strBaseColour);
-}
-
-console.log("App code started");
+document.getElementById("y").onclick = function(e) { segment_click("y"); }
+document.getElementById("y1").onclick = function(e) { segment_click("y"); }
+document.getElementById("yg").onclick = function(e) { segment_click("yg"); }
+document.getElementById("yg1").onclick = function(e) { segment_click("yg"); }
+document.getElementById("g").onclick = function(e) { segment_click("g"); }
+document.getElementById("g1").onclick = function(e) { segment_click("g"); }
+document.getElementById("bg").onclick = function(e) { segment_click("bg"); }
+document.getElementById("bg1").onclick = function(e) { segment_click("bg"); }
+document.getElementById("b").onclick = function(e) { segment_click("b"); }
+document.getElementById("b1").onclick = function(e) { segment_click("b"); }
+document.getElementById("bv").onclick = function(e) { segment_click("bv"); }
+document.getElementById("bv1").onclick = function(e) { segment_click("bv"); }
+document.getElementById("v").onclick = function(e) { segment_click("v"); }
+document.getElementById("v1").onclick = function(e) { segment_click("v"); }
+document.getElementById("rv").onclick = function(e) { segment_click("rv"); }
+document.getElementById("rv1").onclick = function(e) { segment_click("rv"); }
+document.getElementById("r").onclick = function(e) { segment_click("r"); }
+document.getElementById("r1").onclick = function(e) { segment_click("r"); }
+document.getElementById("ro").onclick = function(e) { segment_click("ro"); }
+document.getElementById("ro1").onclick = function(e) { segment_click("ro"); }
+document.getElementById("o").onclick = function(e) { segment_click("o"); }
+document.getElementById("o1").onclick = function(e) { segment_click("o"); }
+document.getElementById("yo").onclick = function(e) { segment_click("yo"); }
+document.getElementById("yo1").onclick = function(e) { segment_click("yo"); }
